66ecce0ac4fed67898bac9fcf1a18f3e9fb3c775
João de Sousa Gonçalves <js.goncalves@campus.fct.unl.pt>
2024-04-08 15:38:14 +0100

    a

--- src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/LoginResource.java
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/LoginResource.java
@@ -1,5 +1,27 @@
 package pt.unl.fct.di.apdc.firstwebapp.resources;
 
+import java.util.logging.Logger;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.Consumes;
+
+@Path("/login")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class LoginResource {
 
+    private static final Logger LOG = Logger.getLogger(LoginResource.class.getName());
+
+    public LoginResource() {}
+
+
+    @POST
+    @Path("/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response doLogin(LoginData data) {
+        return Response.ok().build();
+    }
 }
